0f070e48211b0436ef4117c6dfb4f93a25f0bd79
Shiv Bhonde <shivbhonde04@gmail.com>
2024-05-25 18:04:33 +0530

    update home page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,65 +1,205 @@
 "use client";
 
-import Link from "next/link";
+import { useCallback, useState } from "react";
+import { ZKEdDSAEventTicketPCDPackage } from "@pcd/zk-eddsa-event-ticket-pcd";
+import { zuAuthPopup } from "@pcd/zuauth";
 import type { NextPage } from "next";
+import { hexToBigInt } from "viem";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
+import { notification } from "~~/utils/scaffold-eth";
+import { generateWitness, isETHBerlinPublicKey } from "~~/utils/scaffold-eth/pcd";
+import { ETHBERLIN_ZUAUTH_CONFIG } from "~~/utils/zupassConstants";
+
+// Get a valid event id from { supportedEvents } from "zuauth" or https://api.zupass.org/issue/known-ticket-types
+const fieldsToReveal = {
+  revealAttendeeEmail: true,
+  revealEventId: true,
+  revealProductId: true,
+};
 
 const Home: NextPage = () => {
+  const [verifiedFrontend, setVerifiedFrontend] = useState(false);
+  const [verifiedBackend, setVerifiedBackend] = useState(false);
+  const [verifiedOnChain, setVerifiedOnChain] = useState(false);
   const { address: connectedAddress } = useAccount();
+  const [pcd, setPcd] = useState<string>();
+
+  const getProof = useCallback(async () => {
+    if (!connectedAddress) {
+      notification.error("Please connect wallet");
+      return;
+    }
+    const result = await zuAuthPopup({ fieldsToReveal, watermark: connectedAddress, config: ETHBERLIN_ZUAUTH_CONFIG });
+    if (result.type === "pcd") {
+      setPcd(JSON.parse(result.pcdStr).pcd);
+    } else {
+      notification.error("Failed to parse PCD");
+    }
+  }, [connectedAddress]);
+
+  const verifyProofFrontend = async () => {
+    if (!pcd) {
+      notification.error("No PCD found!");
+      return;
+    }
+
+    if (!connectedAddress) {
+      notification.error("Please connect wallet");
+      return;
+    }
+    const deserializedPCD = await ZKEdDSAEventTicketPCDPackage.deserialize(pcd);
+
+    if (!(await ZKEdDSAEventTicketPCDPackage.verify(deserializedPCD))) {
+      notification.error(`[ERROR Frontend] ZK ticket PCD is not valid`);
+      return;
+    }
+
+    if (!isETHBerlinPublicKey(deserializedPCD.claim.signer)) {
+      notification.error(`[ERROR Frontend] PCD is not signed by ETHBerlin`);
+      return;
+    }
+
+    if (deserializedPCD.claim.watermark.toString() !== hexToBigInt(connectedAddress as `0x${string}`).toString()) {
+      notification.error(`[ERROR Frontend] PCD watermark doesn't match`);
+      return;
+    }
+
+    setVerifiedFrontend(true);
+    notification.success(
+      <>
+        <p className="font-bold m-0">Frontend Verified!</p>
+        <p className="m-0">
+          The proof has been verified
+          <br /> by the frontend.
+        </p>
+      </>,
+    );
+  };
+
+  const sendPCDToServer = async () => {
+    let response;
+    try {
+      response = await fetch("/api/verify", {
+        method: "POST",
+        body: JSON.stringify({
+          pcd: pcd,
+          address: connectedAddress,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (e) {
+      notification.error(`Error: ${e}`);
+      return;
+    }
+
+    const data = await response.json();
+    setVerifiedBackend(true);
+    notification.success(
+      <>
+        <p className="font-bold m-0">Backend Verified!</p>
+        <p className="m-0">{data?.message}</p>
+      </>,
+    );
+  };
+
+  // mintItem verifies the proof on-chain and mints an NFT
+  const { writeContractAsync: mintNFT, isPending: isMintingNFT } = useScaffoldWriteContract("YourCollectible");
+
+  const { data: yourBalance } = useScaffoldReadContract({
+    contractName: "YourCollectible",
+    functionName: "balanceOf",
+    args: [connectedAddress],
+  });
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+      <div className="flex flex-col items-center mt-24">
+        <div className="card max-w-[90%] sm:max-w-lg bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Zupass: Scaffold-ETH 2 Starter Kit</h2>
+            <p className="mt-0">
+              Get started with{" "}
+              <a className="link" href="https://github.com/proofcarryingdata/zupass" target="_blank">
+                Zupass
+              </a>{" "}
+              to verify PCDs (Proof-Carrying Data). <span className="font-bold">e.g.</span> ETHBerlin tickets.
+            </p>
+            <p className="text-sm m-0">
+              - Check
+              <code className="mx-1 px-1 italic bg-base-300 font-bold max-w-full break-words break-all inline-block">
+                packages/nextjs/pages/index.tsx
+              </code>
+              to learn how to ask Zupass for a zero knowledge proof.
+            </p>
+            <p className="text-sm m-0">
+              - Check
+              <code className="mx-1 px-1 italic bg-base-300 font-bold max-w-full break-words break-all inline-block">
+                packages/nextjs/pages/api/verify.tsx
+              </code>
+              to learn how to verify the proof on the backend and execute any action (in this example it will send 1 ETH
+              to the connected address).
+            </p>
+            <div className="flex flex-col gap-4 mt-6">
+              <div className="tooltip" data-tip="Loads the Zupass UI in a modal, where you can prove your PCD.">
+                <button className="btn btn-secondary w-full tooltip" onClick={getProof} disabled={!!pcd}>
+                  {!pcd ? "1. Get Proof" : "1. Proof Received!"}
+                </button>
+              </div>
+              <div className="tooltip" data-tip="When you get back the PCD, verify it on the frontend.">
+                <button
+                  className="btn btn-primary w-full"
+                  disabled={!pcd || verifiedFrontend}
+                  onClick={verifyProofFrontend}
+                >
+                  2. Verify (frontend)
+                </button>
+              </div>
+              <div className="tooltip" data-tip="Send the PCD to the server to verify it and execute any action.">
+                <button
+                  className="btn btn-primary w-full"
+                  disabled={!verifiedFrontend || verifiedBackend}
+                  onClick={sendPCDToServer}
+                >
+                  3. Verify (backend) and send ETH
+                </button>
+              </div>
+              <div className="tooltip" data-tip="Submit the proof to a smart contract to verify it on-chain.">
+                <button
+                  className="btn btn-primary w-full"
+                  disabled={!verifiedBackend || verifiedOnChain}
+                  onClick={async () => {
+                    try {
+                      await mintNFT({
+                        functionName: "mintItem",
+                        // @ts-ignore TODO: fix the type later with readonly fixed length bigInt arrays
+                        args: [pcd ? generateWitness(JSON.parse(pcd)) : undefined],
+                      });
+                    } catch (e) {
+                      notification.error(`Error: ${e}`);
+                      return;
+                    }
+                    setVerifiedOnChain(true);
+                  }}
+                >
+                  {isMintingNFT ? <span className="loading loading-spinner"></span> : "4. Verify (on-chain) and mint"}
+                </button>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  className="btn btn-ghost text-error underline normal-case"
+                  onClick={() => {
+                    setVerifiedFrontend(false);
+                  }}
+                >
+                  Reset
+                </button>
+              </div>
+              <div className="text-center text-xl">
+                {yourBalance && yourBalance >= 1n ? "🎉 🍾 proof verified in contract!!! 🥂 🎊" : ""}
+              </div>
             </div>
           </div>
         </div>
